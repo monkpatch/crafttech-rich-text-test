3897f31ee406397f374890721e287d81eb40bccd
Ivan Petrukhin <petruekhin@yandex.ru>
2025-03-13 05:05:09 +0300

    BaseURL

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  base: 'http://monkpatch.github.io/crafttech-rich-text-test',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
